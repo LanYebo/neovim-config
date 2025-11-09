6889f23f959de06ed3675da81999a367d54b3093
lanyebo <lanyebig@foxmail.com>
2025-11-09 21:52:12 +0800

    <fix> 修复lsp按键映射配置警告
    lsp按键映射警告某个方法过时，进行更新

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,14 @@
 return {
   -- my custom keymaps for lsp
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- disable default key
-      keys[#keys + 1] = { "K", false }
-      -- cuttom my own keymaps
-      keys[#keys + 1] = { "K", "5k", desc = "quick move up 5 lines" }
-      keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "lsp hover" }
-    end,
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "K", false }, -- 禁用 LSP 默认的 K -> hover
+          { "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", desc = "LSP Hover", has = "hover" },
+        },
+      },
+    },
   },
 }
